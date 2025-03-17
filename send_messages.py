1abc0f9db517dcbab61d3aaafdd3c8b3c5bc8af0
ansu223 <alipress.shin@gmail.com>
2025-03-17 22:59:07 +0000

    Update send_messages.py

--- send_messages.py
+++ send_messages.py
@@ -4,7 +4,7 @@ from telegram import Bot, InlineKeyboardButton, InlineKeyboardMarkup
 BOT_TOKEN = '7432253073:AAFTts6hbQ0ehD9D_uyGG9MBeYiLBsO4HOg'
 
 # Replace with your chat ID
-YOUR_CHAT_ID = '823543373'  # Your chat ID
+YOUR_CHAT_ID = '1626072171'  # Your chat ID
 
 # Initialize the bot
 bot = Bot(token=BOT_TOKEN)
